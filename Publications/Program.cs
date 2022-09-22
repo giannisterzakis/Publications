5a7851f3438355c2ce72e002f4bb313faef1c6b9
Giannis Terzakis <59915445+giannisterzakis@users.noreply.github.com>
2022-09-23 01:12:09 +0300

    Add files via upload

---
+++ Publications/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Publications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Publications";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+            Book book1 = new Book("Software Development", 10.99, 3, "Liz Gandy");
+            BulkBook book2 = new BulkBook("Programming in C#", 20.5, 1, "Linda White");
+
+            Magazine mag1 = new Magazine("C# monthly", 2.5, 10, 10, "September");
+            DiscMag dmag1 = new DiscMag("C# programming", 2.5, 10, 10, "September");
+
+            // Display the Title, Author and the copies of the book
+            Console.WriteLine(book1.Title + " " + book1.Author + " " + book1.Copies);
+            Console.WriteLine(book2.Title + " " + book2.Author + " " + book2.Copies);
+            Console.WriteLine(mag1.Title + " " + mag1.CurrIssue + " " + mag1.Copies);
+            Console.WriteLine(dmag1.Title + " " + dmag1.CurrIssue + " " + dmag1.Copies);
+            Console.WriteLine();
+
+            book2.SellCopy();
+            mag1.SellCopy();
+            mag1.SellCopy();
+
+            // Display the Title, Author and the new amount of copies of the book
+            Console.WriteLine(book1.Title + " " + book1.Author + " " + book1.Copies);
+            Console.WriteLine(book2.Title + " " + book2.Author + " " + book2.Copies);
+            Console.WriteLine(mag1.Title + " " + mag1.CurrIssue + " " + mag1.Copies);
+            Console.WriteLine(dmag1.Title + " " + dmag1.CurrIssue + " " + dmag1.Copies);
+            Console.WriteLine();
+
+            mag1.RecvNewIssue("October");
+
+            Console.WriteLine("Receiving a new issue of the DiscMag");
+            dmag1.RecvNewIssue("October");
+            Console.WriteLine();
+
+            book2.BulkOrder();
+
+            // Display the Title, Author and the new amount of copies of the book
+            Console.WriteLine(book1.Title + " " + book1.Author + " " + book1.Copies);
+            Console.WriteLine(book2.Title + " " + book2.Author + " " + book2.Copies);
+            Console.WriteLine(mag1.Title + " " + mag1.CurrIssue + " " + mag1.Copies);
+            Console.WriteLine(dmag1.Title + " " + dmag1.CurrIssue + " " + dmag1.Copies);
+            Console.WriteLine();
+
+            // ToString() methods have been invoked for the output of the details
+            Console.WriteLine(book1.ToString());
+            Console.WriteLine(book2.ToString());
+            Console.WriteLine(mag1.ToString());
+            Console.WriteLine(dmag1.ToString());
+
+
+            //Exit from application
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Press any key to exit: ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Publications/Publication.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Publications
+{
+    class Publication
+    {
+        private String title;
+
+        public String Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        private int copies;
+
+        public int Copies
+        {
+            get { return copies; }
+            set { copies = value; }
+        }
+
+        public Publication(String title, double price, int copies)
+        {
+            this.title = title;
+            this.price = price;
+            this.copies = copies;
+        }
+
+        // Sell one copy
+        public void SellCopy()
+        {
+            copies--;
+        }
+
+        // ToString() method has been declared 
+        public override String ToString()
+        {
+            return title;
+        }
+    }
+
+    // Subclass of Publication is defined
+    class Book : Publication
+    {
+        private String author;
+
+        public String Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        // Constructor of this class 
+        public Book(String title, double price, int copies, String author) : base(title, price, copies)
+        {
+            this.author = author;
+        }
+
+        public void OrderCopies(int copies)
+        {
+            Copies = Copies + copies;
+        }
+
+        // ToString() method has been declared 
+        public override String ToString()
+        {
+            return base.ToString() + " by " + author;
+        }
+    }
+
+    // Subclass of Publication is defined
+    class Magazine : Publication
+    {
+        private int orderQty;
+
+        public int OrderQty
+        {
+            get { return orderQty; }
+            set { orderQty = value; }
+        }
+
+        private String currIssue;
+
+        public String CurrIssue
+        {
+            get { return currIssue; }
+            set { currIssue = value; }
+        }
+
+        // Constructor of this class 
+        public Magazine(String title, double price, int copies, int orderQty,
+                String currIssue)
+            : base(title, price, copies)
+        {
+            this.orderQty = orderQty;
+            this.currIssue = currIssue;
+        }
+
+
+        // Adjust quantity
+        public void AdjustQty(int qty)
+        {
+            orderQty = qty;
+        }
+
+        // Receive new issue
+        public virtual void RecvNewIssue(String newIssue)
+        {
+            Copies = orderQty;
+            currIssue = newIssue;
+        }
+
+        // ToString() method has been declared 
+        public override String ToString()
+        {
+            return base.ToString() + " (" + currIssue + ")";
+        }
+    }
+
+    class DiscMag : Magazine
+    {
+        // Constructor of this class 
+        public DiscMag(String title, double price, int copies, int orderQty,
+                String currIssue)
+            : base(title, price, copies, orderQty, currIssue)
+        {
+
+        }
+
+        // Receive new issue
+        public override void RecvNewIssue(String newIssue)
+        {
+            base.RecvNewIssue(newIssue);
+            Console.WriteLine("Check discs attached to this magazine");
+        }
+    }
+
+    class BulkBook : Book
+    {
+        const int BATCH_SIZE = 100;
+
+        // Constructor of this class 
+        public BulkBook(String title, double price, int copies, String author)
+            : base(title, price, copies, author)
+        {
+        }
+
+        public void BulkOrder()
+        {
+            Copies = Copies + BATCH_SIZE;
+        }
+    }
+}
